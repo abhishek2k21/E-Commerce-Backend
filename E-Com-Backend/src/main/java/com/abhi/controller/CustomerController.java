998a363238d321f1650a3622a824faf74e648646
Abhishek Kumar <123145812+abhishek2k21@users.noreply.github.com>
2024-10-20 15:41:16 +0530

    Update CustomerController.java

--- E-Com-Backend/src/main/java/com/abhi/controller/CustomerController.java
+++ E-Com-Backend/src/main/java/com/abhi/controller/CustomerController.java
@@ -30,7 +30,6 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerService;
 	
-	// Handler to get a list of all customers
 	
 	@GetMapping("/customers")
 	public ResponseEntity<List<Customer>> getAllCustomersHandler(@RequestHeader("token") String token){
@@ -38,7 +37,6 @@ public class CustomerController {
 	}
 	
 	
-	// Handler to Get a customer details of currently logged in user - sends data as per token
 	
 	@GetMapping("/customer/current")
 	public ResponseEntity<Customer> getLoggedInCustomerDetailsHandler(@RequestHeader("token") String token){
@@ -46,7 +44,6 @@ public class CustomerController {
 	}
 	
 	
-	// Handler to Update a customer
 	
 	@PutMapping("/customer")
 	public ResponseEntity<Customer> updateCustomerHandler(@Valid @RequestBody CustomerUpdateDTO customerUpdate, @RequestHeader("token") String token){
@@ -54,41 +51,35 @@ public class CustomerController {
 	}
 	
 	
-	// Handler to update a customer email-id or mobile no
 	@PutMapping("/customer/update/credentials")
 	public ResponseEntity<Customer> updateCustomerMobileEmailHandler(@Valid @RequestBody CustomerUpdateDTO customerUpdate, @RequestHeader("token") String token){
 		return new ResponseEntity<>(customerService.updateCustomerMobileNoOrEmailId(customerUpdate, token), HttpStatus.ACCEPTED);
 	}
 	
 	
-	// Handler to update customer password
 	@PutMapping("/customer/update/password")
 	public ResponseEntity<SessionDTO> updateCustomerPasswordHandler(@Valid @RequestBody CustomerDTO customerDto, @RequestHeader("token") String token){		
 		return new ResponseEntity<>(customerService.updateCustomerPassword(customerDto, token), HttpStatus.ACCEPTED);
 	}
 	
 	
-	// Handler to Add or update new customer Address
 	@PutMapping("/customer/update/address")
 	public ResponseEntity<Customer> updateAddressHandler(@Valid @RequestBody Address address, @RequestParam("type") String type, @RequestHeader("token") String token){
 		return new ResponseEntity<>(customerService.updateAddress(address, type, token), HttpStatus.ACCEPTED);
 	}
 	
 	
-	// Handler to update Credit card details
 	@PutMapping("/customer/update/card")
 	public ResponseEntity<Customer> updateCreditCardHandler(@RequestHeader("token") String token, @Valid @RequestBody CreditCard newCard){
 		return new ResponseEntity<>(customerService.updateCreditCardDetails(token, newCard), HttpStatus.ACCEPTED);
 	}
 	
 	
-	// Handler to Remove a user address
 	@DeleteMapping("/customer/delete/address")
 	public ResponseEntity<Customer> deleteAddressHandler(@RequestParam("type") String type, @RequestHeader("token") String token){
 		return new ResponseEntity<>(customerService.deleteAddress(type, token), HttpStatus.ACCEPTED);
 	}
 	
-	// Handler to delete customer
 	@DeleteMapping("/customer")
 	public ResponseEntity<SessionDTO> deleteCustomerHandler(@Valid @RequestBody CustomerDTO customerDto, @RequestHeader("token") String token){
 		return new ResponseEntity<>(customerService.deleteCustomer(customerDto, token), HttpStatus.ACCEPTED);
